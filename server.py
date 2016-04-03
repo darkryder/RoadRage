b8db2e364ca113689a5c91006d503a1c8c6b9a3e
Sambhav Satija <sambhav13085@iiitd.ac.in>
2016-04-03 18:11:00 +0530

    Added api endpoint which specifies whether the user is in an accident prone area

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from utils import coord_distance, _sort_accidents_dist
 HEATMAP_RADIUS = 150
 HEATMAP_MIN_ACCIDENTS = 100
 
+PRONE_AREA_THRESHOLD = 95
+PRONE_AREA_DISTANCE_ACCIDENTS_CHECK = 150
+
 data = None
 with open("random.json", 'r') as f:
     data = json.loads(f.read())
@@ -61,6 +64,27 @@ def nearest_accident():
     nearest_accident['longitude'] = nearest_accident['location']['longitude']
 
     return json.dumps(nearest_accident)
+
+@app.route("/analyse_and_warn")
+def core_analyse():
+    lat = request.args.get('lat', None)
+    lon = request.args.get('lon', None)
+    if lat is None or lon is None:
+        return "Parameters not provided", 403
+
+    lat, lon = map(float, (lat, lon))
+
+    # get all near accidents within PRONE_AREA_DISTANCE_ACCIDENTS_CHECK distance
+    near_accidents = consider_accidents = sorted(data, key=_sort_accidents_dist(lat, lon))
+    score = 0.0
+    for count, accident in enumerate(near_accidents):
+        dist = coord_distance(lat, lon, **accident['location'])
+        if dist > PRONE_AREA_DISTANCE_ACCIDENTS_CHECK:
+            break
+        score += accident['damageRating']
+    print str(score) + " with %d accidents" % count
+    in_accident_prone_area = score > PRONE_AREA_THRESHOLD
+    return json.dumps({'in_accident_prone_area': in_accident_prone_area})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
